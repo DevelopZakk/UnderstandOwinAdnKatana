108498b683eafff40e0a7f966bdd769bf8ff2531
Develop Zakk <DevelopZakk@outlook.com>
2017-11-07 16:31:46 +0800

    selfhost owin

---
+++ OwinSelfHostDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace OwinSelfHostDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:12345"))
+            {
+                Console.WriteLine("Listen to port 12345");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ OwinSelfHostDemo/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using Owin;
+
+namespace OwinSelfHostDemo
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+
+            app.Use(async (ctx, next) =>
+            {
+                await ctx.Response.WriteAsync("Hello World");
+            });
+        }
+
+    }
+}
