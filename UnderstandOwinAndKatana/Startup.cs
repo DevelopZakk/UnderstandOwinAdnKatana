c2409fe2f9db3f9b1e41e1d4cfac255b05b1b3dd
Develop Zakk <DevelopZakk@outlook.com>
2017-11-07 11:11:25 +0800

    init projects

---
+++ UnderstandOwinAndKatana/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+
+namespace UnderstandOwinAndKatana
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+
+            app.Use(async (ctx,next) =>
+            {
+                await ctx.Response.WriteAsync($"<html><body><h1>Hello World</h1></body></html>");
+            });
+        }
+
+    }
+}
