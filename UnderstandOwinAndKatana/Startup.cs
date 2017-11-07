4fad7754d44fb98a88c98fe29aa7c000daea0d3e
Develop Zakk <DevelopZakk@outlook.com>
2017-11-07 11:16:00 +0800

    owin pipeline add new middleware

--- UnderstandOwinAndKatana/Startup.cs
+++ UnderstandOwinAndKatana/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Owin;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices.ComTypes;
+using Owin;
 
 namespace UnderstandOwinAndKatana
 {
@@ -6,10 +8,17 @@ namespace UnderstandOwinAndKatana
     {
         public static void Configuration(IAppBuilder app)
         {
-
+            app.Use(async (ctx, next) =>
+            {
+                Debug.WriteLine("Incoming Request:" +ctx.Request.Path);
+                await next();
+                Debug.WriteLine("Outogint Request:" + ctx.Request.Path);
+            });
             app.Use(async (ctx,next) =>
             {
+                Debug.WriteLine("Incoming Request 2module:" + ctx.Request.Path);
                 await ctx.Response.WriteAsync($"<html><body><h1>Hello World</h1></body></html>");
+                Debug.WriteLine("Outgoingcoming Request 2module:" + ctx.Request.Path);
             });
         }
 
